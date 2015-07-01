32bff54a7c963e67d281e9be3d5bfc980eddb90d
arashyousefi <arashyou3fi@gmail.com>
2015-07-01 16:37:04 +0430

    test

---
+++ src/Test.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+import gamePanel.BozorgMenuBar;
+import gamePanel.GamePanel;
+import bozorg.judge.Judge;
+
+public class Test {
+	static int[][] cellTypes, wallTypes;
+	static int[] players;
+
+	public static void main(String[] args) {
+		readMap();
+		Judge judge = new Judge();
+		GamePanel panel = new GamePanel();
+		judge.loadMap(cellTypes, wallTypes, players);
+		panel.init(judge);
+		panel.setJMenuBar(new BozorgMenuBar(judge));
+		panel.pack();
+		panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.setVisible(true);
+	}
+
+	public static void readMap() {
+		File map = new File("resources/map.txt");
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(map);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int width = scanner.nextInt();
+		int height = scanner.nextInt();
+		cellTypes = new int[height][width];
+		wallTypes = new int[height][width];
+		for (int i = 0; i < height; ++i)
+			for (int j = 0; j < width; ++j)
+				wallTypes[i][j] = scanner.nextInt();
+		for (int i = 0; i < height; ++i)
+			for (int j = 0; j < width; ++j)
+				cellTypes[i][j] = scanner.nextInt();
+		int playercnt = scanner.nextInt();
+		players = new int[playercnt];
+		for (int i = 0; i < playercnt; ++i)
+			players[i] = scanner.nextInt();
+	}
+}
